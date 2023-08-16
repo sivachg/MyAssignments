faeac2484fc5bc368b0dbab6442fdfb60c1f0ec3
Sivachidambaram G <53971445+sivachg@users.noreply.github.com>
2023-08-16 20:21:49 +0530

    Add files via upload
    Day 4 Assignments - Create Lead, Edit Lead

---
+++ day4/EditLead.java
@@ -0,0 +1,36 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class EditLead {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/logout");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("username")).sendKeys("demoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		String title = driver.getTitle();
+		System.out.println(title);
+		driver.findElement(By.className("crmsfa")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestTape");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Regina");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Thomas");
+		driver.findElement(By.name("submitButton")).click();
+		driver.findElement(By.linkText("Edit")).click();
+		driver.findElement(By.id("updateLeadForm_companyName")).clear();
+		driver.findElement(By.id("updateLeadForm_companyName")).sendKeys("VRL Exports");
+		driver.findElement(By.name("submitButton")).click();
+		String text = driver.findElement(By.id("viewLead_companyName_sp")).getText();
+		System.out.println(text);
+
+	}
+
+}
