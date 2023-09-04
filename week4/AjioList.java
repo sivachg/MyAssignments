89f68d7adeb318c980f09bcae3c4c1885c67e487
Sivachidambaram G <53971445+sivachg@users.noreply.github.com>
2023-09-04 20:56:31 +0530

    Add files via upload

---
+++ week4/AjioList.java
@@ -0,0 +1,64 @@
+package week4.day2;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class AjioList {
+
+	public static void main(String[] args) throws InterruptedException {
+		//Chrome driver setup
+		WebDriverManager.chromedriver().setup();
+		//create object for chrome driver
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--disable-notifications");
+		ChromeDriver driver = new ChromeDriver(options);
+		//maximize window
+		driver.manage().window().maximize();
+		//get web page url
+		driver.get("https://www.ajio.com/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		WebElement searchBar = driver.findElement(By.xpath("//div[@class='searchbar-view']//input"));
+		searchBar.sendKeys("bags");
+		searchBar.sendKeys(Keys.ENTER);
+		Thread.sleep(4000);
+		driver.findElement(By.xpath("(//div[@class='facet-linkhead'])[3]//label")).click();
+		Thread.sleep(6000);
+		driver.findElement(By.xpath("(//div[@class='facet-linkhead'])[6]//label")).click();
+		Thread.sleep(3000);
+		System.out.println(driver.findElement(By.xpath("//div[@class='length']")).getText());
+		List<WebElement> brand = driver.findElements(By.xpath("//div[@class='brand']"));
+		System.out.println(brand.size());
+		List<String> brandList = new ArrayList<String>();
+		for (int i = 1; i <= brand.size(); i++) {
+			String text = driver.findElement(By.xpath("//div[@class='brand']")).getText();
+			brandList.add(text);
+		}
+		//System.out.println("Brands available in the page : " +brandList);
+		Set<String> brandSet = new TreeSet<String>(brandList);
+		for (String string : brandSet) {
+			System.out.println(string);
+		} 
+		List<WebElement> name = driver.findElements(By.xpath("//div[@class='nameCls']"));
+		List<String> nameList = new ArrayList<String>();
+		for (int i = 1; i <= name.size(); i++) {
+			String text = driver.findElement(By.xpath("//div[@class='nameCls']")).getText();
+			nameList.add(text);
+		}
+		//System.out.println("Name of bags : " +nameList);
+		Set<String> nameSet = new TreeSet<String>(nameList);
+		for (String string : nameSet) {
+			System.out.println(string);
+		} 
+	}
+
+}
